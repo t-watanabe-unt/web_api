8e3233903d28d2b2513d346cabbddec5f0cfc53e
tooiea <87057731+tooiea@users.noreply.github.com>
2023-04-08 00:50:16 +0900

    文書ファイルのダウンロード処理実装

--- src/core/app/Http/Controllers/api/DocumentsFilesController.php
+++ src/core/app/Http/Controllers/api/DocumentsFilesController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DocumentDownloadRequest;
+use App\Models\Document;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DocumentsFilesController extends Controller
 {
-    //
-    public function download()
+    /**
+     * Undocumented function
+     *
+     * @param  DocumentDownloadRequest $request
+     * @param  string                  $document_number
+     * @return \Symfony\Component\HttpFoundation\StreamedResponse
+     */
+    public function download(DocumentDownloadRequest $request, $document_number)
     {
         // ファイルのダウンロード
+        $document = Document::where('document_number', '=', $document_number)->first();
+        $headers = [['Content-Type' => $document->document_mime_type]];
+
+        return Storage::download($document->document_path, $document->document_name, $headers);
     }
 
     public function updateFile()
--- src/core/app/Http/Requests/DocumentDownloadRequest.php
+++ src/core/app/Http/Requests/DocumentDownloadRequest.php
@@ -3,29 +3,11 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\DocumentDeleteRequest;
 
 /**
  * 文書のファイル取得時のバリデーション
  */
-class DocumentDownloadRequest extends FormRequest
+class DocumentDownloadRequest extends DocumentDeleteRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
 }
