7cb89ae3fed29fb1e4fc21e0878c88d998bf1dd9
touya watanabe <t-watanabe@un-t.com>
2023-04-17 19:58:03 +0900

    途中分コミット

--- src/core/tests/Feature/Api/Error/DocumentFileDownloadErrorTest.php
+++ src/core/tests/Feature/Api/Error/DocumentFileDownloadErrorTest.php
@@ -4,17 +4,42 @@ namespace Tests\Feature\Api\Error;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\DocumentCommonFunctionsTest;
 use Tests\TestCase;
 
-class DocumentFileDownloadErrorTest extends TestCase
+class DocumentFileDownloadErrorTest extends DocumentCommonFunctionsTest
 {
     /**
-     * A basic feature test example.
+     * 文書番号が存在しない
      */
-    public function test_example(): void
+    public function test_no_exist_document_number(): void
     {
-        $response = $this->get('/');
+        $root = sprintf("%s/%s/file", self::ROOT_DOCUMENT, 'aa5bffed-89eb-41ca-94f2-607e9d971503');
+        $response = $this->deleteJson($root);
+        $response->assertStatus(self::CODE_400);
+    }
 
-        $response->assertStatus(200);
+    /**
+     * 文書番号の形式が異なる(uuid:ver4)
+     * 文書番号のレコードが存在している
+     */
+    public function test_regex_document_number(): void
+    {
+        $document = $this->registerDocumentBeforeTest();
+        $root = sprintf("%s/%s/file", self::ROOT_DOCUMENT, 'aa5bf-edd89eb-41ca-94f2-607e9d971503');
+        $response = $this->deleteJson($root);
+        $response->assertStatus(self::CODE_400);
+
+        $this->deleteDocumentAfterTest($document->document_number);
+    }
+
+    /**
+     * 文書番号なし
+     */
+    public function test_no_document_number(): void
+    {
+        $root = self::ROOT_DOCUMENT;
+        $response = $this->deleteJson($root);
+        $response->assertStatus(self::CODE_405);
     }
 }
